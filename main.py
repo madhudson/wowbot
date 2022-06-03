c6a1a41d3ff1eaf33ad7eacb3770057a3c6fdafd
jibbajabba <jamie.hudson1@gmail.com>
2022-06-03 07:47:52 +0000

    asynchttp

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import asyncio
+import aiohttp
 import discord
 import os
 import time
@@ -12,19 +13,26 @@ class WoWBot(discord.Client):
       'gif'
     ]
     self.channel_id = None
+    self.last_dungeon = None
+    self.raiderio_url = 'https://raider.io/api/v1/characters/profile?region=eu&realm=tarren%20mill&name=turdbrush&fields=mythic_plus_recent_runs'
 
-  async def test(self):
+  async def external_request(self, uri=None):
+    with aiohttp.ClientSession() as session:
+      async with session.get(uri) as resp:
+        data = await resp.json()
+        print(data)
+  
+  async def raiderio(self):
     while True:
-      time.sleep(5)
+      time.sleep(3)
       if not self.channel_id:
         for chan in self.get_all_channels():
           if chan.name == 'general':
             self.channel_id = chan.id
       else:
         chan = self.get_channel(self.channel_id)
-        print(chan)
-        await chan.send('testing this out')
-        return
+        data = await self.external_request(self.raiderio_url)
+        print(data)
 
 
   async def on_ready(self):
@@ -33,7 +41,7 @@ class WoWBot(discord.Client):
   async def on_message(self, message):
     if message.author == self.user:
       return
-    asyncio.ensure_future(self.test())
+    asyncio.ensure_future(self.raiderio())
     print('tis done')
 
 
