8299cace6c6e05fc4e39aa806335abbf3e722680
jibbajabba <jamie.hudson1@gmail.com>
2022-06-03 07:35:51 +0000

    asyncio.ensure_future

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
+import asyncio
 import discord
 import os
 import time
-import threading
 
 
 class WoWBot(discord.Client):
@@ -11,22 +11,31 @@ class WoWBot(discord.Client):
     self.commands = [
       'gif'
     ]
+    self.channel_id = None
 
   async def test(self):
     while True:
-      time.sleep(3)
-      channels = 
-      
+      time.sleep(5)
+      if not self.channel_id:
+        for chan in self.get_all_channels():
+          if chan.name == 'general':
+            self.channel_id = chan.id
+      else:
+        chan = self.get_channel(self.channel_id)
+        print(chan)
+        await chan.send('testing this out')
+        return
+
+
   async def on_ready(self):
     print(f'logged in as {self.user}')
 
   async def on_message(self, message):
     if message.author == self.user:
       return
-    print(f'got message!: {message.content}')
+    asyncio.ensure_future(self.test())
+    print('tis done')
 
 
 wow_bot = WoWBot()
-x = threading.Thread(target=wow_bot.test)
-x.start()
 wow_bot.run(os.environ['DISCORD_TOKEN'])
