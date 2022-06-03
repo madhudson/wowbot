401ee7681ec4251a092ee9b1fe7d5ab1d04b1250
jibbajabba <jamie.hudson1@gmail.com>
2022-06-03 07:21:57 +0000

    things

--- main.py
+++ main.py
@@ -1,12 +1,32 @@
 import discord
 import os
 import time
+import threading
 
 
 class WoWBot(discord.Client):
+  def __init__(self, *args, **kwargs):
+    super(WoWBot, self).__init__(*args, **kwargs)
+    self.prefix = '!'
+    self.commands = [
+      'gif'
+    ]
+
+  async def test(self):
+    while True:
+      time.sleep(3)
+      channels = 
+      
   async def on_ready(self):
     print(f'logged in as {self.user}')
 
+  async def on_message(self, message):
+    if message.author == self.user:
+      return
+    print(f'got message!: {message.content}')
+
 
 wow_bot = WoWBot()
+x = threading.Thread(target=wow_bot.test)
+x.start()
 wow_bot.run(os.environ['DISCORD_TOKEN'])
